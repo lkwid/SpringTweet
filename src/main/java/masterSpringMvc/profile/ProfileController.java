04977009b3fcf471a2edd29a7bcf31173303c7d0
Lukasz Widawski <lk.widawski@gmail.com>
2017-02-28 23:42:51 +0100

    custom error page

--- src/main/java/masterSpringMvc/profile/ProfileController.java
+++ src/main/java/masterSpringMvc/profile/ProfileController.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,6 +16,17 @@ import masterSpringMvc.date.USLocalDateFormatter;
 
 @Controller
 public class ProfileController {
+	private UserProfileSession userProfileSession;
+	
+	@Autowired
+	public ProfileController(UserProfileSession userProfileSession) {
+		this.userProfileSession = userProfileSession;
+	}
+	
+	@ModelAttribute
+	public ProfileForm getProfileForm() {
+		return userProfileSession.toForm();
+	}
 	
 	@ModelAttribute("dateFormat")
 	public String localFormat(Locale locale) {
