efd7548d147c73b7ce61d5ad12accfdf8fa12dcb
Lukasz Widawski <lk.widawski@gmail.com>
2017-03-01 14:32:39 +0100

    array variable search

--- src/main/java/masterSpringMvc/config/WebConfiguration.java
+++ src/main/java/masterSpringMvc/config/WebConfiguration.java
@@ -10,9 +10,11 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.util.UrlPathHelper;
 
 import masterSpringMvc.date.USLocalDateFormatter;
 
@@ -44,6 +46,13 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		UrlPathHelper urlPathHelper = new UrlPathHelper();
+		urlPathHelper.setRemoveSemicolonContent(false);
+		configurer.setUrlPathHelper(urlPathHelper);		
 	}	
 	
 }
