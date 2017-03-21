47c905bbf61782257ed76a656a0130b78ef9846f
Lukasz Widawski <lk.widawski@gmail.com>
2017-03-21 19:39:07 +0100

    new user login form

--- src/main/java/master_spring_mvc/config/ApiSecurityConfiguration.java
+++ src/main/java/master_spring_mvc/config/ApiSecurityConfiguration.java
@@ -2,6 +2,7 @@ package master_spring_mvc.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,8 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@Order(1)
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
@@ -22,11 +24,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.antMatcher("/api/**")
 		.httpBasic()
 		.and()
 		.csrf().disable()
-		.authorizeRequests()
-		.antMatchers("/login", "/logout").permitAll()
+		.authorizeRequests()		
 		.antMatchers(HttpMethod.GET, "/api/**").hasRole("USER")
 		.antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
 		.antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
